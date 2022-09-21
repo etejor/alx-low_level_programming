66e2877ecddbd9bf9c21166d8bdd510dea88c9cd
etejor <ethomas.ejor@gmail.com>
2022-09-21 16:13:25 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+/**
+*_strncat -concatenate twostrings but add inputted number of bytes
+*@dest: string to be appended upon
+*@src: strings to be completed at the end of dest
+*@n:integer parameter to compare index to
+*Return: returns new concatenated string
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
